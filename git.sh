611322aa39d2bcbaeba987ba282635af01fd8796
Enrico Gabrielli <enricogabrielli76.peragr@gmail.com>
2023-01-19 23:08:22 +0100

    metriche resistenza

--- git.sh
+++ git.sh
@@ -1,5 +1,5 @@
 #git init
 git add --all
-git commit -m "metriche rmarkdown"
+git commit -m "metriche resistenza"
 #git remote add origin "url del proprio repository github"
 git push -u origin main
